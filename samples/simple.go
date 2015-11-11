78516d87430480c2c06fc9753180fa13649c193f
zlbbq <zlbbq@163.com>
2015-11-11 14:27:06 +0800

    add samples

--- samples/simple.go
+++ samples/simple.go
@@ -10,4 +10,8 @@ func main() {
 	logger.Warn("Hello, %s", "World")
 	logger.Error("Hello, %s", "World")
 	logger.Fatal("Hello, %s", "World")
+
+	// It is strongly recommended that call logger.Get() function to get a logger from logger pool
+	moduleLogger := logger.Get("module_full_name")
+	moduleLogger.Info("This is a module logger, it is able to be controlled by application entry")
 }
